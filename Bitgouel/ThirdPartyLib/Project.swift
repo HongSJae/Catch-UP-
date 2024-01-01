ee713a0486a24255911588d936150e620ec2b2b9
HongSJae <gtw030488@gmail.com>
2024-01-01 23:42:12 +0900

    🧩 :: ThirdPartyLib

---
+++ Bitgouel/ThirdPartyLib/Project.swift
@@ -0,0 +1,13 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.dynamicFramework(
+    name: "ThirdPartyLib",
+    packages: [],
+    deploymentTarget: .iOS(targetVersion: "15.9", devices: .iphone),
+    dependencies: [
+        .SPM.Moya,
+        .SPM.NeedleFoundation
+    ],
+    scripts: [.SwiftLintString]
+)
