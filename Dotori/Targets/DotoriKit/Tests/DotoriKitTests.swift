745f494e7be5aea1d21f7e71d0cf2f7e85aedad6
HongSJae <gtw030488@gmail.com>
2024-01-01 16:28:48 +0900

    🧩 :: Dotori Tuist init

---
+++ Dotori/Targets/DotoriKit/Tests/DotoriKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class DotoriKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("DotoriKit", "DotoriKit")
+    }
+}
---
+++ Dotori/Targets/DotoriUI/Tests/DotoriUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class DotoriUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("DotoriUI", "DotoriUI")
+    }
+}
