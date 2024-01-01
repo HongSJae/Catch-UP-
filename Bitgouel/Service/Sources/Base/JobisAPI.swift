980ff08842a6452a5a7652e533fc63552b08d347
HongSJae <gtw030488@gmail.com>
2024-01-01 23:41:39 +0900

    🧩 :: Service

---
+++ Bitgouel/Service/Sources/Base/JobisAPI.swift
@@ -0,0 +1,40 @@
+import Foundation
+import Moya
+
+public protocol JobisAPI: TargetType, JwtAuthorizable {
+    associatedtype ErrorType: Error
+    var domain: JobisDomain { get }
+    var urlPath: String { get }
+    var errorMap: [Int: ErrorType] { get }
+}
+
+public extension JobisAPI {
+    var baseURL: URL {
+        URL(
+            string: Bundle.main.object(forInfoDictionaryKey: "APBASE_URL") as? String ?? ""
+        ) ?? URL(string: "https://www.google.com")!
+    }
+
+    var path: String {
+        domain.asURLString + urlPath
+    }
+
+    var headers: [String: String]? {
+        ["Content-Type": "application/json"]
+    }
+
+    var validationType: ValidationType {
+        return .successCodes
+    }
+}
+
+public enum JobisDomain: String {
+    case users
+    case recruitments
+}
+
+extension JobisDomain {
+    var asURLString: String {
+        "/\(self.rawValue)"
+    }
+}
