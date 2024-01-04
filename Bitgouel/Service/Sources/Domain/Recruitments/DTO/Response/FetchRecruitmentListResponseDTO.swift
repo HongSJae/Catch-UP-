9040e160e5f16c7481b2a432b0e4f9895a4a436d
HongSJae <gtw030488@gmail.com>
2024-01-04 09:46:34 +0900

    🧩 :: RecruitmentEntity/ResponseDTO

---
+++ Bitgouel/Service/Sources/Domain/Recruitments/DTO/Response/FetchRecruitmentListResponseDTO.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+struct RecruitmentListResponseDTO: Decodable {
+    let recruitments: [RecruitmentResponseDTO]
+}
+
+struct RecruitmentResponseDTO: Codable {
+    let recruitID: Int
+    let companyName: String
+    let companyProfileURL: String
+    let trainPay: Int
+    let militarySupport: Bool
+    let hiringJobs: String
+    let bookmarked: Bool
+
+    enum CodingKeys: String, CodingKey {
+        case recruitID = "id"
+        case companyName = "company_name"
+        case companyProfileURL = "company_profile_url"
+        case trainPay = "train_pay"
+        case militarySupport = "military_support"
+        case hiringJobs = "hiring_jobs"
+        case bookmarked
+    }
+}
+
+extension RecruitmentListResponseDTO {
+    func toDomain() -> [RecruitmentEntity] {
+        recruitments.map {
+            RecruitmentEntity(
+                recruitID: $0.recruitID,
+                companyName: $0.companyName,
+                companyProfileURL: $0.companyProfileURL,
+                trainPay: $0.trainPay,
+                militarySupport: $0.militarySupport,
+                hiringJobs: $0.hiringJobs,
+                bookmarked: $0.bookmarked
+            )
+        }
+    }
+}
---
+++ Bitgouel/Service/Sources/Domain/Recruitments/Entity/RecruitmentEntity.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+public struct RecruitmentEntity: Equatable, Hashable {
+    public let recruitID: Int
+    public let companyName: String
+    public let companyProfileURL: String
+    public let trainPay: Int
+    public let militarySupport: Bool
+    public let hiringJobs: String
+    public let bookmarked: Bool
+
+    init(recruitID: Int, companyName: String, companyProfileURL: String, trainPay: Int, militarySupport: Bool, hiringJobs: String, bookmarked: Bool) {
+        self.recruitID = recruitID
+        self.companyName = companyName
+        self.companyProfileURL = companyProfileURL
+        self.trainPay = trainPay
+        self.militarySupport = militarySupport
+        self.hiringJobs = hiringJobs
+        self.bookmarked = bookmarked
+    }
+}
