f9a3e927f77666123abe9e629d076644ba5558e4
HongSJae <gtw030488@gmail.com>
2024-01-13 23:52:34 +0900

    🧩 :: Plugin 추가

--- Dotori/Tuist/Config.swift
+++ Dotori/Tuist/Config.swift
@@ -2,6 +2,9 @@ import ProjectDescription
 
 let config = Config(
     plugins: [
-        .local(path: .relativeToManifest("../../Plugins/Dotori")),
-    ]
+        .local(path: .relativeToRoot("Plugin/DependencyPlugin")),
+        .local(path: .relativeToRoot("Plugin/ConfigurationPlugin")),
+        .local(path: .relativeToRoot("Plugin/EnvironmentPlugin"))
+    ],
+    generationOptions: .options()
 )
